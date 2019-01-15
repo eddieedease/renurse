9f62d3d0fac3096446cc5c9836a5377107a763b9
Eddie <eddie@edease.nl>
2019-01-15 11:48:32 +0100

    preparingapi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,34 @@
-import { Component, TemplateRef } from '@angular/core';
+import { Component, TemplateRef } from "@angular/core";
 
-import { Subscription } from 'rxjs';
- 
-import { EdserService } from './edser.service';
+import { Subscription } from "rxjs";
 
-import {
-  Router,
-  ActivatedRoute,
-  Params
-} from '@angular/router';
+import { EdserService } from "./edser.service";
+
+import { Router, ActivatedRoute, Params } from "@angular/router";
 
 // modal service
-import {
-  BsModalService
-} from 'ngx-bootstrap/modal';
-import {
-  BsModalRef
-} from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { BsModalService } from "ngx-bootstrap/modal";
+import { BsModalRef } from "ngx-bootstrap/modal/bs-modal-ref.service";
 
-import {
-  ToastrService
-} from 'ngx-toastr';
+import { ToastrService } from "ngx-toastr";
 
-import {
-  environment
-} from '../environments/environment';
+import { environment } from "../environments/environment";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'Renurse';
+  title = "Renurse";
   subscription: Subscription;
 
   // Var for showing hiding header/footer
   dMin = false;
 
-   // user creds
-   usrId = '';
-   usrPwd = '';
+  // user creds
+  usrId = "";
+  usrPwd = "";
 
   modalRef: BsModalRef;
 
@@ -48,40 +36,44 @@ export class AppComponent {
 
   showLoginSpinner = false;
 
-  constructor(private edSer: EdserService, private thisrouter: Router, private modalService: BsModalService, private toastr: ToastrService) {
-      this.subscription = this.edSer.getdMin().subscribe(bool => { this.dMin = bool; });
-      this.router = thisrouter;
+  constructor(
+    private edSer: EdserService,
+    private thisrouter: Router,
+    private modalService: BsModalService,
+    private toastr: ToastrService
+  ) {
+    this.subscription = this.edSer.getdMin().subscribe(bool => {
+      this.dMin = bool;
+    });
+    this.router = thisrouter;
 
-      if (environment.production === false) {
-        this.doTestCall();
-      } else {
-        
-      }
+    if (environment.production === false) {
+      this.doTestCall();
+    } else {
+    }
   }
 
   doTestCall() {
     this.edSer.API_testCall().subscribe(value => this.gotTest(value));
-
   }
 
-  gotTest(_resp){
+  gotTest(_resp) {
     this.edSer.debugLog(_resp);
   }
 
-
-  openModal(template: TemplateRef < any > ) {
+  openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
 
   loginAttempt() {
     // check some thing and  enable spinning loader
-    if (this.usrId !== '' && this.usrPwd !== '') {
+    if (this.usrId !== "" && this.usrPwd !== "") {
       // adding a fake timer
       this.showLoginSpinner = true;
-        setTimeout(() => {
-           this.gotLoginResponse('OK');
-       }, 1000);
-      
+      setTimeout(() => {
+        this.gotLoginResponse('OK');
+      }, 1000);
+
       // TODO: enable API CALL
       // this.edSer.API_login(this.usrId, this.usrPwd).subscribe(value => this.gotLoginResponse(value));
     }
@@ -91,10 +83,9 @@ export class AppComponent {
   gotLoginResponse(_event) {
     this.showLoginSpinner = false;
     this.modalRef.hide();
-    this.router.navigate(['shop']);
-    
-    this.toastr.success('Welkom', 'succes!');
-    this.router.navigate(['loggedin']);
-  }
+    this.router.navigate(["shop"]);
 
+    this.toastr.success("Welkom", "succes!");
+    this.router.navigate(["loggedin"]);
+  }
 }
